10efbb5d6b55d1cc4c0a2528b68b53461a180746
trygfmi <117619880+trygfmi@users.noreply.github.com>
2026-03-16 19:08:20 +0900

    シェルスクリプト内の不要なコードを削除

--- start_shellscript_get-error-log.sh
+++ start_shellscript_get-error-log.sh
@@ -3,9 +3,5 @@
 # "$(find . -type f -name "start_shellscript_get-error-log.sh")"
 
 
-shopt -s expand_aliases
-source ~/.bash_profile
-
 echo "hello shellscript" >> log/command.log 2>> error_log/command_error.log
 ls --false >> log/command.log 2>> error_log/command_error.log
-
